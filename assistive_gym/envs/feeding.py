76cee35ce76b59fe6f871c808b5ecf93534b63ac
csmile-1006 <changyeon.kim@kaist.ac.kr>
2024-11-22 13:57:33 -0600

    change details for feeding

--- assistive_gym/envs/feeding.py
+++ assistive_gym/envs/feeding.py
@@ -333,7 +333,7 @@ class FeedingEnv(AssistiveEnv):
         self.target_pos = np.array(target_pos)
         sphere_collision = -1
         sphere_visual = p.createVisualShape(
-            shapeType=p.GEOM_SPHERE, radius=0.01, rgbaColor=[0, 1, 0, 1], physicsClientId=self.id
+            shapeType=p.GEOM_SPHERE, radius=0.02, rgbaColor=[0, 1, 0, 1], physicsClientId=self.id
         )
         self.target = p.createMultiBody(
             baseMass=0.0,
--- assistive_gym/envs/human_creation.py
+++ assistive_gym/envs/human_creation.py
@@ -110,7 +110,7 @@ class HumanCreation:
                 length=0.253 / 8,
                 position_offset=[0.253 / 2.5 - 0.253 / 16, 0, 0],
                 orientation=p.getQuaternionFromEuler([0, np.pi / 2.0, 0], physicsClientId=self.id),
-                color=[0, 1, 1, 1],
+                color=[0, 1, 1, 1] if self.cloth else [0.8, 0.6, 0.4, 1],
             )
             neck_c, neck_v = create_body(
                 shape=p.GEOM_CAPSULE,
@@ -129,7 +129,7 @@ class HumanCreation:
                 radius=0.043 * rs,
                 length=0,
                 position_offset=[0, 0, -0.043 * rs],
-                color=[1, 0, 1, 1],
+                color=[1, 0, 1, 1] if self.cloth else [0.8, 0.6, 0.4, 1],
             )
             right_hand_c, right_hand_v = create_body(
                 shape=p.GEOM_SPHERE, radius=0.043 * rs, length=0, position_offset=[0, 0, -0.043 * rs]
@@ -166,7 +166,7 @@ class HumanCreation:
                 shapeType=p.GEOM_SPHERE,
                 radius=(0.043 + 0.033) / 2 * rs,
                 # length=0,
-                rgbaColor=[0, 1, 0, 1],  # Bright vivid green color for contrast
+                rgbaColor=[0, 1, 0, 1] if self.cloth else [0.8, 0.6, 0.4, 1],  # Bright vivid green color for contrast
                 visualFramePosition=[0, 0.01, 0],
                 physicsClientId=self.id,
             )
@@ -181,7 +181,12 @@ class HumanCreation:
                 )
                 shoulder_cloth_c, _ = create_body(shape=p.GEOM_SPHERE, radius=0.043 * rs, length=0)
                 # elbow_cloth_c, _ = create_body(shape=p.GEOM_SPHERE, radius=0.043*rs, length=0)
-                elbow_cloth_c, _ = create_body(shape=p.GEOM_SPHERE, radius=0.043 * rs, length=0, color=[0, 1, 0, 1])
+                elbow_cloth_c, _ = create_body(
+                    shape=p.GEOM_SPHERE,
+                    radius=0.043 * rs,
+                    length=0,
+                    color=[0, 1, 0, 1] if self.cloth else [0.8, 0.6, 0.4, 1],
+                )
                 wrist_cloth_c, _ = create_body(shape=p.GEOM_SPHERE, radius=0.033 * rs, length=0)
 
             head_scale = [0.89] * 3
@@ -249,7 +254,7 @@ class HumanCreation:
                 length=0.225 / 8,
                 position_offset=[0.225 / 2.5 - 0.225 / 16, 0, 0],
                 orientation=p.getQuaternionFromEuler([0, np.pi / 2.0, 0], physicsClientId=self.id),
-                color=[0, 1, 1, 1],
+                color=[0, 1, 1, 1] if self.cloth else [0.8, 0.6, 0.4, 1],
             )
             neck_c, neck_v = create_body(
                 shape=p.GEOM_CAPSULE,
@@ -268,7 +273,7 @@ class HumanCreation:
                 radius=0.0355 * rs,
                 length=0,
                 position_offset=[0, 0, -0.0355 * rs],
-                color=[1, 0, 1, 1],
+                color=[1, 0, 1, 1] if self.cloth else [0.8, 0.6, 0.4, 1],
             )
             right_hand_c, right_hand_v = create_body(
                 shape=p.GEOM_SPHERE, radius=0.0355 * rs, length=0, position_offset=[0, 0, -0.0355 * rs]
@@ -304,8 +309,7 @@ class HumanCreation:
                 shapeType=p.GEOM_SPHERE,
                 radius=(0.0355 + 0.027) / 2 * rs,
                 length=0,
-                # rgbaColor=[0.8, 0.6, 0.4, 1],
-                rgbaColor=[0, 1, 0, 1],  # Bright vivid green color for contrast
+                rgbaColor=[0, 1, 0, 1] if self.cloth else [0.8, 0.6, 0.4, 1],  # Bright vivid green color for contrast
                 visualFramePosition=[0, 0.01, 0],
                 physicsClientId=self.id,
             )
@@ -320,7 +324,12 @@ class HumanCreation:
                 )
                 shoulder_cloth_c, _ = create_body(shape=p.GEOM_SPHERE, radius=0.0355 * rs, length=0)
                 # elbow_cloth_c, _ = create_body(shape=p.GEOM_SPHERE, radius=0.0355*rs, length=0)
-                elbow_cloth_c, _ = create_body(shape=p.GEOM_SPHERE, radius=0.0355 * rs, length=0, color=[0, 1, 0, 1])
+                elbow_cloth_c, _ = create_body(
+                    shape=p.GEOM_SPHERE,
+                    radius=0.0355 * rs,
+                    length=0,
+                    color=[0, 1, 0, 1] if self.cloth else [0.8, 0.6, 0.4, 1],
+                )
                 wrist_cloth_c, _ = create_body(shape=p.GEOM_SPHERE, radius=0.027 * rs, length=0)
 
             head_scale = [0.89] * 3
