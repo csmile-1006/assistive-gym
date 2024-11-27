5b434942b9daeb55e6e0e936f14c1b3b2e181345
csmile-1006 <doli1573@kaist.ac.kr>
2024-11-27 01:03:57 +0000

    reduce resolution size

--- assistive_gym/envs/env.py
+++ assistive_gym/envs/env.py
@@ -83,8 +83,8 @@ class AssistiveEnv(gym.Env):
         self.record_video = False
         self.video_writer = {key: {"name": None, "writer": None} for key in ["front", "top", "side"]}
 
-        self.width = 1920
-        self.height = 1080
+        self.width = 1920 // 8
+        self.height = 1080 // 8
 
         # Configure the camera with each viewpoint (front, right) using setup_camera_rpy
         camera_kwargs = dict(
