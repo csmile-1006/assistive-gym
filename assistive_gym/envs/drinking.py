7c39893dd0bc64adbe2aca9ff55d05e47c89b488
csmile-1006 <changyeon.kim@kaist.ac.kr>
2025-02-12 13:27:01 -0600

    minor color change

--- assistive_gym/envs/drinking.py
+++ assistive_gym/envs/drinking.py
@@ -532,7 +532,7 @@ class DrinkingEnv(AssistiveEnv):
         )
         self.waters = list(range(last_water_id - water_count + 1, last_water_id + 1))
         for w in self.waters:
-            p.changeVisualShape(w, -1, rgbaColor=[0.25, 0.5, 1, 1], physicsClientId=self.id)
+            p.changeVisualShape(w, -1, rgbaColor=[0, 1, 1, 1], physicsClientId=self.id)
         self.total_water_count = len(self.waters)
 
         # Enable rendering
