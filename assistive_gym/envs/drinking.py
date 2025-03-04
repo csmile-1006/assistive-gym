aa0d6482bbb89b7c56b81fa8adae77546de3caa5
csmile-1006 <cykim1006@gmail.com>
2025-03-04 20:19:39 +0000

    change weight for tilting: as it's too low

--- assistive_gym/envs/drinking.py
+++ assistive_gym/envs/drinking.py
@@ -749,7 +749,7 @@ class DrinkingEnv(AssistiveEnv):
             "r_water_transfer": Box(low=1.0, high=1.0, shape=(), dtype=float),
             # Other terms: non-negative, strictly less than primary
             "r_cup_distance": Box(low=1.0, high=1.0, shape=(), dtype=float),
-            "r_cup_tilting": Box(low=0.0, high=0.1, shape=(), dtype=float),
+            "r_cup_tilting": Box(low=0.0, high=0.5, shape=(), dtype=float),
             "r_spillage": Box(low=0.0, high=1.0, shape=(), dtype=float),
             "r_contact": Box(low=0.0, high=0.1, shape=(), dtype=float),
             "r_jerky": Box(low=0.0, high=1.0, shape=(), dtype=float),
